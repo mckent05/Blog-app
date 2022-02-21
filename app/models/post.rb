50a80bb7631bc23b19bea0578a649908278d2289
MCKENT05 <atemitope95@gmail.com>
2022-02-21 20:12:30 +0100

    corrected assoications between authors and Posts, comments and likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 
-  belongs_to :user, class_name: 'User', foreign_key: 'users_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'users_id'
 
   def comment_5_recent
     comments.order('created_at desc').limit(5)
