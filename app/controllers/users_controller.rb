25db98a1882a134664581744ab31d8beee428891
MCKENT05 <atemitope95@gmail.com>
2022-02-23 07:46:52 +0100

    fixed linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all 
+    @user = User.all
   end
 
   def show
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 end
