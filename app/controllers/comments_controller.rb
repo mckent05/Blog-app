d2a5597cd56414e16e29e8c742b531074efb2e78
MCKENT05 <atemitope95@gmail.com>
2022-02-24 20:12:27 +0100

    fixed error on comments and likes controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    new_comment = Comment.new(author: current_user, post:, text: comment_params)
+    user_post = Post.find(params[:post_id])
+    new_comment = Comment.new(author: current_user, post: user_post, text: comment_params)
     new_comment.update_comment_counter
 
     respond_to do |format|
       format.html do
         if new_comment.save
           flash[:success] = 'Comment succesful'
-          redirect_to user_post_path(current_user.id, post.id)
+          redirect_to user_post_path(current_user.id, user_post.id)
         else
           render :new, alert: 'Error occured!'
         end
