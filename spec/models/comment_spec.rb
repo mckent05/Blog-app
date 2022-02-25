4fee955671b8b2cb5e20a9ab200213d390bf9636
MCKENT05 <atemitope95@gmail.com>
2022-02-25 11:50:59 +0100

    finished validation, model testing and solved n+1 problem

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'test for the comments mode should pass' do
+    new_user = User.new(name: 'Akinlade Temitope', bio: 'simple guy', postscounter: 2)
+    new_post = Post.new(author: new_user, text: 'Nice post', commentscounter: 3, likescounter: 5)
+    subject do
+      Comment.new
+    end
+
+    before { subject.save }
+
+    it 'should be inavlid because text is nil' do
+      subject.text = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should be invalid because of the length of text' do
+      subject.text = 'a' * 250
+      expect(subject).to_not be_valid
+    end
+
+    it 'should return 4' do
+      subject.post = new_post
+      subject.update_comment_counter
+      expect(subject.post.commentscounter).to be(4)
+    end
+  end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'test for the comments mode should pass' do
+    new_user = User.new(name: 'Akinlade Temitope', bio: 'simple guy', postscounter: 2)
+    new_post = Post.new(author: new_user, text: 'Nice post', commentscounter: 3, likescounter: 5)
+    subject do
+      Like.new
+    end
+
+    before { subject.save }
+
+    it 'should return 4' do
+      subject.post = new_post
+      subject.author = new_user
+      subject.likes_counter_update
+      expect(subject.post.likescounter).to be(6)
+    end
+  end
 end
