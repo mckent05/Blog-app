8c940fea160a0ad36ac17383dc3966418cc73709
MCKENT05 <atemitope95@gmail.com>
2022-03-01 13:02:35 +0100

    fixed linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @user = User.all
   end
@@ -8,5 +8,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
 end
