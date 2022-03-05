29af6db838c3d85ee9e559e90dea74d383ba7d26
MCKENT05 <atemitope95@gmail.com>
2022-03-05 09:02:54 +0100

    fixed linter errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::UsersController < ApplicationController
-    # before_action :authenticate_user!
-  
-    def show
-      @user = User.find(params[:id])
-      render json: @user, status: :ok
-    end
+  # before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user, status: :ok
+  end
 end
